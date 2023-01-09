3dfc511d7359729e17cf2c9cd33dc72b290e574e
##韩淞霖## <1411905376@qq.com>
2023-01-09 16:29:41 +0800

    添加test_06

---
+++ src/test_07/shader/fragment.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+in vec4 ourcolor;
+in vec2 TexCoord;
+// uniform vec4 vColor;
+uniform sampler2D ourTextuer;
+
+void main(){
+    // FragColor = vec4(ourcolor.x, ourcolor.y+vColor.y, ourcolor.z, 1.0f);
+    // FragColor = vec4(ourcolor.x, ourcolor.y, ourcolor.z, 1.0f);
+    // FragColor = vec4(ourcolor);
+    FragColor = texture(ourTextuer, TexCoord) * vec4(ourcolor);
+}
---
+++ src/test_07/shader/vertex.glsl
@@ -0,0 +1,19 @@
+#version 330 core 
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 acolor;
+layout (location = 2) in vec2 aTexCoord;
+
+out vec4 ourcolor;
+out vec2 TexCoord;
+uniform float offsetx;
+uniform float offsety;
+
+void main(){
+    // aPos.x = aPos.x+offset;
+    // gl_Position = vec4(aPos, 1.0f);
+    gl_Position = vec4(aPos.x + offsetx, aPos.y + offsety , aPos.z, 1.0f);
+    // gl_Position = vec4(aPos.x, aPos.y , aPos.z, 1.0f);
+    ourcolor = acolor;
+    TexCoord = aTexCoord;
+    gl_PointSize = 20.0f;
+}
